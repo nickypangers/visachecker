0c250520d37196e4658b39c649ba03dcfd541691
nickypangers <nixonpang98@gmail.com>
2020-06-05 17:11:14 +0800

    Added swipe to delete and navigate to search on tap

--- lib/friends.dart
+++ lib/friends.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'search.dart';
 import 'services/friendObject.dart';
 import 'drawer.dart';
 import 'services/SearchList.dart';
@@ -16,6 +18,10 @@ class _FriendsScreenState extends State<FriendsScreen> {
 
   List<Friend> friends = [];
 
+  _setDesCountry(String value) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('desCountry', value);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,56 +70,61 @@ class _FriendsScreenState extends State<FriendsScreen> {
                           icon: Icon(Icons.add),
                           onPressed: () {
                             showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return AlertDialog(
-                                  title: Text("Add Friend"),
-                                  content: Column(
-                                    children: [
-                                      TextField(
-                                        controller: _nameController,
-                                        decoration: InputDecoration(
-                                          hintText: 'Enter Name',
+                              barrierDismissible: false,
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text("Add Friend"),
+                                    content: Column(
+                                      children: [
+                                        TextField(
+                                          controller: _nameController,
+                                          decoration: InputDecoration(
+                                            hintText: 'Enter Name',
+                                          ),
                                         ),
+                                        TextField(
+                                          controller: _locController,
+                                          decoration: InputDecoration(
+                                            hintText: 'Enter Country',
+                                          ),
+                                          onTap: () {
+                                            showSearch(
+                                                context: context,
+                                                delegate: DataSearch(
+                                                    controller:
+                                                        _locController));
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                        child: Text("Back"),
+                                        onPressed: () {
+                                          _nameController.clear();
+                                          _locController.clear();
+                                          Navigator.of(context).pop();
+                                        },
                                       ),
-                                      TextField(
-                                        controller: _locController,
-                                        decoration: InputDecoration(
-                                          hintText: 'Enter Country',
-                                        ),
-                                        onTap: () {
-                                          showSearch(
-                                                  context: context,
-                                                  delegate: DataSearch(
-                                                      controller: _locController));
+                                      FlatButton(
+                                        child: Text("Add"),
+                                        onPressed: () {
+                                          setState(() {
+                                            friends.add(
+                                              Friend(
+                                                  name: _nameController.text,
+                                                  country: _locController.text),
+                                            );
+                                          });
+                                          _nameController.clear();
+                                          _locController.clear();
+                                          Navigator.of(context).pop();
                                         },
                                       ),
                                     ],
-                                  ),
-                                  actions: <Widget>[
-                                    FlatButton(
-                                      child: Text("Back"),
-                                      onPressed: () {
-                                        _nameController.clear();
-                                        _locController.clear();
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                    FlatButton(
-                                      child: Text("Add"),
-                                      onPressed: () {
-                                        setState(() {
-                                          friends.add(Friend(name: _nameController.text, country: _locController.text),);
-                                        });
-                                        _nameController.clear();
-                                        _locController.clear();
-                                        Navigator.of(context).pop();
-                                      },
-                                    ),
-                                  ],
-                                );
-                              }
-                            );
+                                  );
+                                });
                           },
                         ),
                       ],
@@ -127,10 +138,61 @@ class _FriendsScreenState extends State<FriendsScreen> {
             child: ListView.builder(
               itemCount: friends.length,
               itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(friends[index].name),
-                  subtitle: Text(friends[index].country),
-                  onTap: () {},
+                return Dismissible(
+                  key: Key(friends[index].name),
+                  direction: DismissDirection.endToStart,
+                  confirmDismiss: (DismissDirection direction) {
+                    return showDialog(
+                        context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Are you sure?"),
+                          content: Text("You are about to remove ${friends[index].name} from your friends list, continue?"),
+                          actions: [
+                            FlatButton(
+                              child: Text("No"),
+                              onPressed: () {
+                                Navigator.of(context).pop(false);
+                              },
+                            ),
+                            FlatButton(
+                              child: Text("Yes"),
+                              onPressed: () {
+                                Navigator.of(context).pop(true);
+                              },
+                            )
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  onDismissed: (direction) {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text("${friends[index].name} dismissed.")));
+                        setState(() {
+                          friends.removeAt(index);
+                        });
+                  },
+                  background: Container(
+                    padding: EdgeInsets.only(
+                      right: 10,
+                    ),
+                    alignment: Alignment.centerRight,
+                    child: Icon(
+                      Icons.delete,
+                      color: Colors.white,
+                    ),
+                    color: Colors.red,
+                  ),
+                  child: ListTile(
+                    title: Text(friends[index].name),
+                    subtitle: Text(friends[index].country),
+                    trailing: Icon(Icons.navigate_next),
+                    onTap: () {
+                      _setDesCountry(friends[index].country);
+                      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SearchScreen()));
+                    },
+                  ),
                 );
               },
             ),
