c866a747139828230df6d77a2c8209388f9c9465
nickypangers <nixon.pang@sgwireless.com>
2020-06-11 17:39:45 +0800

    Revert "new splash and intro screens designs"
    This reverts commit 2476182ac9c1c0899359f809d044d83051aa9199.

--- lib/splash.dart
+++ lib/splash.dart
@@ -15,13 +15,11 @@ class _SplashScreenState extends State<SplashScreen> {
     bool _seen = prefs.getBool('seen');
     print("seen: $_seen");
     if (_seen == null) {
-      Future.delayed(const Duration(seconds: 15));
       Navigator.pushReplacement(
           context,
           PageRouteBuilder(
               pageBuilder: (context, animation1, animation2) => IntroScreen()));
     } else {
-      Future.delayed(const Duration(seconds: 15));
       Navigator.pushReplacement(
           context,
           PageRouteBuilder(
@@ -38,30 +36,10 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color(0xFF00D46D),
-        body: Center(
-            child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 144,
-              width: 144,
-              child: Image.asset("assets/launcher/icon-144.png"),
-            ),
-            Text(
-              "Visa Checker",
-              style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: CircularProgressIndicator(),
-            )
-          ],
-        )));
+      body: Center(
+        child: Text("Loading..."),
+      ),
+    );
   }
 }
 
@@ -85,89 +63,40 @@ class _IntroScreenState extends State<IntroScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF00D46D),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
-            height: 144,
-            width: 144,
-            child: Image.asset("assets/launcher/icon-144.png"),
-          ),
-          Text(
-            "Visa Checker",
-            style: TextStyle(
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
+          Text("Welcome to Visa Checker"),
           Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Padding(
-                padding: EdgeInsets.only(left: 5, top: 15),
-                child: Container(
-                  width: 255,
-                  child: TextField(
-                    style: TextStyle(color: Colors.grey[700]),
-                    cursorColor: Colors.grey[700],
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      errorBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      disabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-                        borderSide: BorderSide.none,
-                      ),
-                      hintText: 'Enter Passport Country',
-                      hintStyle: TextStyle(color: Colors.white),
-                      contentPadding: EdgeInsets.only(
-                          left: 10, right: 10, top: 5, bottom: 5),
-                    ),
-                    onTap: () {
-                      showSearch(
-                          context: context,
-                          delegate: DataSearch(controller: _controller));
-                    },
+              Container(
+                width: 255,
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: "Enter your passport country",
                   ),
+                  controller: _controller,
+                  onTap: () {
+                    showSearch(
+                        context: context,
+                        delegate: DataSearch(controller: _controller));
+                  },
                 ),
               ),
-              IconButton(
-                icon: Icon(Icons.check),
-                alignment: Alignment.center,
-                color: Colors.white,
+              FlatButton(
+                child: Text("Enter"),
                 onPressed: () {
-                  if (_controller.text.length == 0) {
-                    final snackBar = SnackBar(
-                      content: Text("Please enter a country"),
-                    );
-                    snackBar;
-                  } else {
-                    setSeen(true);
-                    setCountry(_controller.text);
-                    Navigator.pushReplacement(
-                        context,
-                        PageRouteBuilder(
-                            pageBuilder: (context, animation1, animation2) =>
-                                HomeScreen()));
-                  }
+                  setSeen(true);
+                  setCountry(_controller.text);
+                  Navigator.pushReplacement(
+                      context,
+                      PageRouteBuilder(
+                          pageBuilder: (context, animation1, animation2) =>
+                              HomeScreen()));
                 },
-              ),
+              )
             ],
           ),
         ],
